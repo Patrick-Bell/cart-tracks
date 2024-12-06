4170949a43c0c479530cbaf612aef59cf827dc46
Patrick Bell <patrick@Patricks-Laptop.local>
2024-12-06 00:17:28 +0000

    update

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'  # Allow frontend in development
+    origins ENV['WEBSITE_URL']  # Allow frontend in development
 
     resource "*",
       headers: :any,
@@ -8,13 +8,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
 
-  # Production: Allow frontend to make requests from the deployed domain
-  allow do
-    origins 'https://cart-tracks.onrender.com'  # Production URL
-
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
 end
